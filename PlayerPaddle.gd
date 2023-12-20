90d8e69af4c42bda417aeb499c5a7238007bd6be
BrandonGroth <bmgroth1988@gmail.com>
2023-12-20 11:51:21 -0500

    Added score UI + reset capabilities

--- PlayerPaddle.gd
+++ PlayerPaddle.gd
@@ -2,9 +2,15 @@ extends CharacterBody2D
 
 @export var PADDLE_SPEED : int = 30
 
+@onready var START_POSITION : Vector2 = global_position
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
 	var direction_y : float = Input.get_axis("move_up", "move_down")
 	velocity = Vector2(0.0, direction_y) * PADDLE_SPEED
 	move_and_collide(velocity*delta)
+	
+func reset() -> void:
+	global_position = START_POSITION
+	velocity = Vector2.ZERO
