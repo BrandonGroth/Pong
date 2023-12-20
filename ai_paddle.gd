9dfd3f926288d75ae4ac3d72784f89f15d499a94
BrandonGroth <bmgroth1988@gmail.com>
2023-12-19 22:47:17 -0500

    Fixed paddle logic + added velocity to ball based on paddle

--- ai_paddle.gd
+++ ai_paddle.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-@export var PADDLE_SPEED = 200
-@export var PADDLE_FRICTION : int = 400
+@export var PADDLE_SPEED = 40
 
 @onready var ball = $"../../Ball/ball"
 
@@ -9,14 +8,8 @@ extends CharacterBody2D
 func _process(delta):
 	# Get direction ball is to correct for it
 	var direction : float = 0.0
-	
 	if ball != null:
-		direction = ball.global_position.y - global_position.y
+		direction = sign(ball.global_position.y - global_position.y)
 	
-	if abs(direction) > 0:
-		move_paddle(direction)
-		
+	velocity = Vector2(0.0, direction) * PADDLE_SPEED
 	move_and_collide(velocity*delta)
-
-func move_paddle(direction_y : float):
-	global_position += Vector2(0.0,direction_y)
